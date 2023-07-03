35f153ccdfba51f06f7d7f169e230ad86f634112
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-07-04 00:32:31 +0330

    feat: subject evaluatiton list

--- src/pages/SubjectsEvaluation/SubjectCard.tsx
+++ src/pages/SubjectsEvaluation/SubjectCard.tsx
@@ -1,78 +1,81 @@
 import { Link } from 'react-router-dom';
+import { useSubjectBasicInfo } from '../../hooks/useSubjectBasicInfo.ts';
+import BrightIdProfilePicture from '../../components/BrightIdProfilePicture.tsx';
 
-export const SubjectCard = ({ subject }: { subject: any }) => {
+export const SubjectCard = ({ subjectId }: { subjectId: string }) => {
+  const { tier, userHasRecovery, name, joinedDateString, auraPublicProfile } =
+    useSubjectBasicInfo(subjectId);
   return (
     <Link
-      to={'/subject/' + subject.id}
-      className={`card card--evaluation b-4 flex !flex-row gap-1 !justify-between w-full ${
-        subject.evaluation === 'POSITIVE'
-          ? '!bg-green-card !opacity-75'
-          : subject.evaluation === 'NEGATIVE'
-          ? '!bg-red-card !opacity-75'
-          : ''
-      }`}
+      to={'/subject/' + subjectId}
+      className={
+        'card card--evaluation b-4 flex !flex-row gap-1 !justify-between w-full'
+        // + subject.evaluation === 'POSITIVE'
+        //   ? '!bg-green-card !opacity-75'
+        //   : subject.evaluation === 'NEGATIVE'
+        //     ? '!bg-red-card !opacity-75'
+        //     : ''
+      }
     >
       <div className="evaluation-left flex flex-col gap-2">
         <div className="evaluation-left__top flex gap-3">
           <div className="evaluation__profile">
-            <img
+            <BrightIdProfilePicture
               className="rounded-full w-12 h-12 border border-white bg-center bg-cover"
-              src={subject.image}
-              alt=""
+              id={subjectId}
             />
           </div>
           <div className="evaluation__info flex flex-col">
-            <p className="text-black font-medium">{subject.name}</p>
+            <p className="text-black font-medium">{name}</p>
             <p className="text-gray20">
-              Tier:{' '}
-              <span className="font-medium text-black">{subject.tier}</span>
+              Tier: <span className="font-medium text-black">{tier}</span>
             </p>
           </div>
         </div>
         <div className="evaluation-left__bottom">
           <p className="text-sm text-gray20">Your evaluation</p>
           <p className="font-medium">
-            {subject.evaluation === 'POSITIVE' ? (
-              <span className="text-green-800">
-                Positive{' '}
-                <span className="text-black">
-                  {' '}
-                  - {subject.evaluationStrength}
-                </span>
-              </span>
-            ) : subject.evaluation === 'NEGATIVE' ? (
-              <span className="text-red-800">
-                Negative{' '}
-                <span className="text-black">
-                  {' '}
-                  - {subject.evaluationStrength}
-                </span>
-              </span>
-            ) : (
-              <span className="text-gray20">...</span>
-            )}
+            {/*{subject.evaluation === 'POSITIVE' ? (*/}
+            {/*  <span className="text-green-800">*/}
+            {/*    Positive{' '}*/}
+            {/*    <span className="text-black">*/}
+            {/*      {' '}*/}
+            {/*      - {subject.evaluationStrength}*/}
+            {/*    </span>*/}
+            {/*  </span>*/}
+            {/*) : subject.evaluation === 'NEGATIVE' ? (*/}
+            {/*  <span className="text-red-800">*/}
+            {/*    Negative{' '}*/}
+            {/*    <span className="text-black">*/}
+            {/*      {' '}*/}
+            {/*      - {subject.evaluationStrength}*/}
+            {/*    </span>*/}
+            {/*  </span>*/}
+            {/*) : (*/}
+            {/*  <span className="text-gray20">...</span>*/}
+            {/*)}*/}
           </p>
         </div>
       </div>
       <div className="evaluation-right flex flex-col gap-2">
         <div className="evaluation-right__top">
           <p className="text-gray20">
-            Score:{' '}
-            <span className="font-medium text-black">{subject.score}</span>
+            Score: <span className="font-medium text-black">{'2.5k'}</span>
           </p>
           <p className="text-gray20">
-            <span className="font-medium text-black">{subject.status}</span>
+            <span className="font-medium text-black">{'18 Pos / 5 Neg'}</span>
           </p>
         </div>
         <div className="evaluation-right__bottom">
           <img
             className="w-26.5 h-12"
             src={
-              subject.evaluation === 'NEGATIVE'
-                ? '/assets/images/negative-chart.svg'
-                : subject.evaluation === 'POSITIVE'
-                ? '/assets/images/positive-chart.svg'
-                : '/assets/images/chart.svg'
+              // subject.evaluation === 'NEGATIVE'
+              //   ? '/assets/images/negative-chart.svg'
+              //   : subject.evaluation === 'POSITIVE'
+              //   ? '/assets/images/positive-chart.svg'
+              //   :
+              '/assets/images/chart.svg'
             }
             alt=""
           />
--- src/pages/SubjectsEvaluation/index.tsx
+++ src/pages/SubjectsEvaluation/index.tsx
@@ -1,40 +1,10 @@
-import { useState } from 'react';
 import { SubjectCard } from './SubjectCard.tsx';
 import { SubjectSearch } from './SubjectSearch.tsx';
+import { selectBrightIdBackup } from '../../store/profile/selectors.ts';
+import { useSelector } from 'react-redux';
 
 const SubjectsEvaluation = () => {
-  const [subjects] = useState<any>([
-    {
-      id: 1,
-      image: '/assets/images/profile.jpeg',
-      name: 'Sina Parvizi',
-      tier: 'Bronze',
-      score: '2.5k',
-      status: '18 Pos / 5 Neg',
-      evaluation: null,
-      evaluationStrength: 'Very High',
-    },
-    {
-      id: 2,
-      image: '/assets/images/profile.jpeg',
-      name: 'Sina Parvizi',
-      tier: 'Bronze',
-      score: '2.5k',
-      status: '18 Pos / 5 Neg',
-      evaluation: 'POSITIVE',
-      evaluationStrength: 'Very High',
-    },
-    {
-      id: 3,
-      image: '/assets/images/profile.jpeg',
-      name: 'Sina Parvizi',
-      tier: 'Bronze',
-      score: '2.5k',
-      status: '18 Pos / 5 Neg',
-      evaluation: 'NEGATIVE',
-      evaluationStrength: 'Medium',
-    },
-  ]);
+  const brightIdBackup = useSelector(selectBrightIdBackup);
 
   return (
     <div className="page page__dashboard">
@@ -42,11 +12,15 @@ const SubjectsEvaluation = () => {
       <p className="text-lg text-white mb-5 mt-7">
         Subjects <strong>(23)</strong>
       </p>
-      <div className="flex flex-col gap-3">
-        {subjects.map((subject: any) => (
-          <SubjectCard subject={subject} key={subject.id} />
-        ))}
-      </div>
+      {brightIdBackup ? (
+        <div className="flex flex-col gap-3">
+          {brightIdBackup.connections.map((conn) => (
+            <SubjectCard subjectId={conn.id} key={conn.id} />
+          ))}
+        </div>
+      ) : (
+        <div>loading...</div>
+      )}
     </div>
   );
 };
