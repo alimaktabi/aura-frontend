94afc7ebb060e7f2e2a29f1ebba67ec8d1294cfc
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-07-05 21:38:29 +0330

    refactor: proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: true,
     proxy: {
@@ -10,8 +10,9 @@ export default defineConfig({
         target: 'https://recovery.brightid.org',
         changeOrigin: true,
         rewrite: (path) => path.slice('/brightid'.length),
+        secure: mode !== 'development',
       },
     },
   },
   plugins: [tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
-});
+}));
