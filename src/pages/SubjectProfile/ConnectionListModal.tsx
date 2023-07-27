d8a22bfd33a24303143bf7516adafa9cf7717c28
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-07-27 21:39:16 +0330

    feat: load on scroll for ConnectionListModal

--- src/pages/SubjectProfile/ConnectionListModal.tsx
+++ src/pages/SubjectProfile/ConnectionListModal.tsx
@@ -1,6 +1,9 @@
 import { ModalItem } from '../../components/Shared/Modal/ModalItem';
 import { SubjectCard } from '../SubjectsEvaluation/SubjectCard';
 import { useInboundConnections } from '../../hooks/useSubjectConnections.ts';
+import InfiniteScroll from 'react-infinite-scroller';
+import { useMemo, useState } from 'react';
+import { BrightIdConnection } from 'types';
 
 export const ConnectionListModal = ({
   subjectId,
@@ -8,16 +11,43 @@ export const ConnectionListModal = ({
   subjectId: string | undefined;
 }) => {
   const { inboundConnections } = useInboundConnections(subjectId);
+  const [items, setItems] = useState<BrightIdConnection[]>([]);
+  const loadMore = (_page: number) => {
+    console.log({ _page });
+    if (!inboundConnections) return;
+    setItems([
+      ...items,
+      ...inboundConnections.slice(items.length, items.length + 10),
+    ]);
+  };
+
+  const hasMore = useMemo(
+    () =>
+      inboundConnections !== null && inboundConnections.length !== items.length,
+    [inboundConnections, items.length],
+  );
+
   return (
     <div className="flex flex-col gap-6 max-h-[600px]">
       <div className="flex w-full justify-between">
         <p>Filters:</p>
         <ModalItem title="Mutual Connections" isSelected={false} />
       </div>
+      {String(hasMore)}
       <div className="flex flex-col overflow-scroll overscroll-contain gap-2.5 w-full -mb-5 pb-5">
-        {inboundConnections?.map((subject) => (
-          <SubjectCard subjectId={subject.id} key={subject.id} />
-        ))}
+        {inboundConnections && (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={loadMore}
+            hasMore={hasMore}
+            initialLoad={true}
+            useWindow={false}
+          >
+            {items.map((subject) => (
+              <SubjectCard subjectId={subject.id} key={subject.id} />
+            ))}
+          </InfiniteScroll>
+        )}
       </div>
     </div>
   );
