71db4e346d8339b6f888366b2a5dfd83bf93603f
Mostafa Asadi <sam.21767@gmail.com>
2023-08-16 18:17:33 +0200

    changed button text color to black, and added Trainers and Managers Evaluation to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,16 @@ import { Link } from 'react-router-dom';
 import { RoutePath } from '../../Routes.tsx';
 
 const Dashboard = () => {
+  const preferredViews = ['Player', 'Trainer', 'Manager'];
+  const preferredView = preferredViews[2];
+  interface stringValue {
+    [key: string]: string;
+  }
+  const preferredViewIcon: stringValue = {
+    Player: '/assets/images/Dashboard/account-icon.svg',
+    Trainer: '/assets/images/Dashboard/trainer-icon.svg',
+    Manager: '/assets/images/Dashboard/manager-icon.svg',
+  };
   return (
     <div className="page page__dashboard">
       <div className="row mb-4">
@@ -15,12 +25,12 @@ const Dashboard = () => {
         <div className="card">
           <p className="mb-5">Preferred view</p>
           <img
-            className="icon mb-7 mx-auto"
-            src="/assets/images/Dashboard/account-icon.svg"
+            className="icon mb-7 mx-auto !w-10 !h-10"
+            src={preferredViewIcon[preferredView]}
             alt=""
           />
           <span className="flex justify-between w-full items-center mt-auto">
-            <p className="font-bold">Player</p>
+            <p className="font-bold">{preferredView}</p>
             <button className="btn btn--icon">
               <img src="/assets/images/Dashboard/refresh-icon.svg" alt="" />
             </button>
@@ -28,7 +38,7 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="row mb-4">
+      <div className="grid grid-cols-2 gap-4">
         <Link to="/domain-overview" className="card">
           <img
             className="icon"
@@ -39,19 +49,56 @@ const Dashboard = () => {
             Domain <br /> Overview
           </p>
         </Link>
-        <Link to="/subjects-evaluation" className="card">
-          <img
-            className="icon"
-            src="/assets/images/Dashboard/subject-evaluation-icon.svg"
-            alt=""
-          />
-          <p className="text-right text-[18px] text-gray20">
-            Subject <br /> Evaluation
-          </p>
-        </Link>
-      </div>
+        {preferredView === 'Player' && (
+          <Link to="/subjects-evaluation" className="card">
+            <img
+              className="icon"
+              src="/assets/images/Dashboard/subject-evaluation-icon.svg"
+              alt=""
+            />
+            <p className="text-right text-[18px] text-gray20">
+              Subjects <br /> Evaluation
+            </p>
+          </Link>
+        )}
+        {preferredView === 'Trainer' && (
+          <Link to="/subjects-evaluation" className="card">
+            <img
+              className="icon"
+              src="/assets/images/Dashboard/account-icon.svg"
+              alt=""
+            />
+            <p className="text-right text-[18px] text-gray20">
+              Players <br /> Evaluation
+            </p>
+          </Link>
+        )}
+        {preferredView === 'Manager' && (
+          <Link to="/subjects-evaluation" className="card">
+            <img
+              className="icon"
+              src="/assets/images/Dashboard/trainer-icon.svg"
+              alt=""
+            />
+            <p className="text-right text-[18px] text-gray20">
+              Trainers <br /> Evaluation
+            </p>
+          </Link>
+        )}
 
-      <div className="row mb-4">
+        {preferredView === 'Manager' && (
+          <Link to="/subjects-evaluation" className="card">
+            <img
+              className="icon"
+              src="/assets/images/Dashboard/manager-icon.svg"
+              alt=""
+            />
+            <p className="text-right text-[18px] text-gray20">
+              Managers <br /> Evaluation
+            </p>
+          </Link>
+        )}
+
         <Link to={RoutePath.PERFORMANCE_OVERVIEW} className="card">
           <img
             className="icon"
@@ -68,7 +115,10 @@ const Dashboard = () => {
             src="/assets/images/Dashboard/setting-icon.svg"
             alt=""
           />
-          <p className="text-right text-[18px] text-gray20 mt-auto">Settings</p>
+          <p className="text-right text-[18px] text-gray20 mt-auto">
+            {' '}
+            <br /> Settings
+          </p>
         </div>
       </div>
     </div>
