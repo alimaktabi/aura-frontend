5bd001db820d76f3f44c1b9a85f2493d36e8e4a1
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-09-15 01:39:59 +0330

    refactor: add brave browser to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'cypress';
+import Browser = Cypress.Browser;
 
 export default defineConfig({
   defaultCommandTimeout: 24000, // 2x average block time
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(_on, config) {
+      const chrome = config.browsers.find(({ name }) => name === 'chrome');
+      if (!chrome) return config;
       return {
         ...config,
         // Only enable Chrome.
-        // Electron (the default) has issues injecting window.ethereum before pageload, so it is not viable.
-        browsers: config.browsers.filter(({ name }) => name === 'chrome'),
+        browsers: [
+          chrome,
+          {
+            ...chrome,
+            name: 'brave',
+            path: 'brave-browser',
+            displayName: 'brave',
+          } as Browser,
+        ],
       };
     },
     baseUrl: 'http://localhost:3000',
