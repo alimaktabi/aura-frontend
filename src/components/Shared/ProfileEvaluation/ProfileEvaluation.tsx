e9b951d0569f59ee51642614da1b3aed99b6e2a3
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-11-29 22:32:40 +0330

    fix: linter

--- src/components/Shared/ProfileEvaluation/ProfileEvaluation.tsx
+++ src/components/Shared/ProfileEvaluation/ProfileEvaluation.tsx
@@ -6,13 +6,10 @@
 // import { selectAuthData } from 'store/profile/selectors';
 // import { connectionLevelIconsBlack } from 'utils/connection';
 // import { compactFormat } from '../../../utils/number';
-import { EvaluatorInfo } from 'components/Shared/EvaluatorInfo';
-import { useSubjectBasicInfo } from 'hooks/useSubjectBasicInfo';
 import { useSubjectEvaluation } from 'hooks/useSubjectEvaluation';
 import { useEffect, useState } from 'react';
 
 import BrightIdProfilePicture from '../../BrightIdProfilePicture';
-import { EvaluationInfo } from '../EvaluationInfo';
 
 const ProfileEvaluation = ({
   fromSubjectId,
@@ -183,29 +180,29 @@ const EvaluatedCardBody = ({
   );
 };
 
-const EvaluatedCardBodyOld = ({
-  fromSubjectId,
-  toSubjectId,
-}: {
-  fromSubjectId: string;
-  toSubjectId: string;
-}) => {
-  const { name } = useSubjectBasicInfo(toSubjectId);
-  return (
-    <>
-      <div className="card__top-row flex justify-between w-full">
-        <EvaluatorInfo evaluatorId={fromSubjectId} />
-        <div className="card__top-row__right flex items-start gap-1.5">
-          <p className="connected-to text-right text-purple text-sm font-medium">
-            evaluated
-            <br /> {name}
-          </p>
-        </div>
-      </div>
-      <EvaluationInfo fromSubjectId={fromSubjectId} toSubjectId={toSubjectId} />
-    </>
-  );
-};
+// const EvaluatedCardBodyOld = ({
+//   fromSubjectId,
+//   toSubjectId,
+// }: {
+//   fromSubjectId: string;
+//   toSubjectId: string;
+// }) => {
+//   const { name } = useSubjectBasicInfo(toSubjectId);
+//   return (
+//     <>
+//       <div className="card__top-row flex justify-between w-full">
+//         <EvaluatorInfo evaluatorId={fromSubjectId} />
+//         <div className="card__top-row__right flex items-start gap-1.5">
+//           <p className="connected-to text-right text-purple text-sm font-medium">
+//             evaluated
+//             <br /> {name}
+//           </p>
+//         </div>
+//       </div>
+//       <EvaluationInfo fromSubjectId={fromSubjectId} toSubjectId={toSubjectId} />
+//     </>
+//   );
+// };
 
 const ConnectedCardBody = ({
   fromSubjectId,
@@ -238,27 +235,27 @@ const ConnectedCardBody = ({
   );
 };
 
-const ConnectedCardBodyOld = ({
-  fromSubjectId,
-  toSubjectId,
-}: {
-  fromSubjectId: string;
-  toSubjectId: string;
-}) => {
-  const { name } = useSubjectBasicInfo(toSubjectId);
-  return (
-    <>
-      <div className="card__top-row flex justify-between items-start w-full">
-        <EvaluatorInfo evaluatorId={fromSubjectId} />
-        <div className="card__top-row__right flex items-stretch gap-1.5">
-          <p className="evaluated text-right text-orange text-sm font-medium">
-            connected to <br /> {name}
-          </p>
-        </div>
-      </div>
-      <EvaluationInfo fromSubjectId={fromSubjectId} toSubjectId={toSubjectId} />
-    </>
-  );
-};
+// const ConnectedCardBodyOld = ({
+//   fromSubjectId,
+//   toSubjectId,
+// }: {
+//   fromSubjectId: string;
+//   toSubjectId: string;
+// }) => {
+//   const { name } = useSubjectBasicInfo(toSubjectId);
+//   return (
+//     <>
+//       <div className="card__top-row flex justify-between items-start w-full">
+//         <EvaluatorInfo evaluatorId={fromSubjectId} />
+//         <div className="card__top-row__right flex items-stretch gap-1.5">
+//           <p className="evaluated text-right text-orange text-sm font-medium">
+//             connected to <br /> {name}
+//           </p>
+//         </div>
+//       </div>
+//       <EvaluationInfo fromSubjectId={fromSubjectId} toSubjectId={toSubjectId} />
+//     </>
+//   );
+// };
 
 export default ProfileEvaluation;
