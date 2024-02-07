79d481ccadbaceb9d8c7684ea24a0617a45c7aaf
unknown <sam.21767@gmail.com>
2024-02-07 12:54:37 +0100

    added border for chart

--- src/contexts/EchartsContext.tsx
+++ src/contexts/EchartsContext.tsx
@@ -14,6 +14,144 @@ interface ProviderProps {
   children: React.ReactNode;
 }
 
+const option2Data = [
+  {
+    value: 9.9,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: -9.2,
+    itemStyle: {
+      color: '#C9A2FF',
+    },
+  },
+  {
+    value: 7.3,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: 5.1,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: 4.1,
+    itemStyle: {
+      color: '#7A7A7A',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: -3.7,
+    itemStyle: {
+      color: '#B5B5B5',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#7A7A7A',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#B5B5B5',
+    },
+  },
+]
+
+const option3Data = [
+  {
+    value: 9.9,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: -9.2,
+    itemStyle: {
+      color: '#C9A2FF',
+    },
+  },
+  {
+    value: 7.3,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: 5.1,
+    itemStyle: {
+      color: '#404040',
+    },
+  },
+  {
+    value: 4.1,
+    itemStyle: {
+      color: '#7A7A7A',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: -3.7,
+    itemStyle: {
+      color: '#B5B5B5',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#7A7A7A',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#E2E2E2',
+    },
+  },
+  {
+    value: 3.7,
+    itemStyle: {
+      color: '#B5B5B5',
+    },
+  },
+]
+
 export const EchartsContextProvider = ({ children }: ProviderProps) => {
   const options = useMemo(
     () => ({
@@ -101,74 +239,13 @@ export const EchartsContextProvider = ({ children }: ProviderProps) => {
       series: [
         {
           color: '#ABCAAE',
-          data: [
-            {
-              value: 9.9,
-              itemStyle: {
-                color: '#404040',
-              },
+          data: option2Data.map(item => ({
+            value: item.value,
+            itemStyle: {
+              ...(item.itemStyle || {}),
+              borderRadius: item.value >= 0 ? [4, 4, 0, 0] : [0, 0, 4, 4],
             },
-            {
-              value: -9.2,
-              itemStyle: {
-                color: '#C9A2FF',
-              },
-            },
-            {
-              value: 7.3,
-              itemStyle: {
-                color: '#404040',
-              },
-            },
-            {
-              value: 5.1,
-              itemStyle: {
-                color: '#404040',
-              },
-            },
-            {
-              value: 4.1,
-              itemStyle: {
-                color: '#7A7A7A',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: -3.7,
-              itemStyle: {
-                color: '#B5B5B5',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#7A7A7A',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#B5B5B5',
-              },
-            },
-          ],
+          })),
           label: {
             show: true,
             position: 'top',
@@ -196,74 +273,13 @@ export const EchartsContextProvider = ({ children }: ProviderProps) => {
       series: [
         {
           color: '#ABCAAE',
-          data: [
-            {
-              value: 9.9,
-              itemStyle: {
-                color: '#404040',
-              },
+          data:  option3Data.map(item => ({
+            value: item.value,
+            itemStyle: {
+              ...(item.itemStyle || {}),
+              borderRadius: item.value >= 0 ? [4, 4, 0, 0] : [0, 0, 4, 4],
             },
-            {
-              value: -9.2,
-              itemStyle: {
-                color: '#C9A2FF',
-              },
-            },
-            {
-              value: 7.3,
-              itemStyle: {
-                color: '#404040',
-              },
-            },
-            {
-              value: 5.1,
-              itemStyle: {
-                color: '#404040',
-              },
-            },
-            {
-              value: 4.1,
-              itemStyle: {
-                color: '#7A7A7A',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: -3.7,
-              itemStyle: {
-                color: '#B5B5B5',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#7A7A7A',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#E2E2E2',
-              },
-            },
-            {
-              value: 3.7,
-              itemStyle: {
-                color: '#B5B5B5',
-              },
-            },
-          ],
+          })),
           label: {
             show: false,
           },
