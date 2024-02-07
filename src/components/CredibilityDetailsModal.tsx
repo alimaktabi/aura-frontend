cc1d11cca17f856866d84382427126ed51226506
unknown <sam.21767@gmail.com>
2024-02-07 12:19:30 +0100

    fixed colors and font style in CredibilityDetailsModal

--- src/components/CredibilityDetailsModal.tsx
+++ src/components/CredibilityDetailsModal.tsx
@@ -29,56 +29,58 @@ const CredibilityDetails = ({ subjectId }: { subjectId: string }) => {
   return (
     <div>
       <div>
-        <div className="font-bold text-l">As a Player:</div>
-        <div>Level: 1</div>
-        <div>Score: 24.1k (28k / -3.9k)</div>
-        <div>Evaluations: 5 (4 Pos / 1 Neg)</div>
-        <div>Your Evaluation: Positive - High (3)</div>
-        <div>Your Evaluation Impact: 32%</div>
+        <div className="font-bold text-l">As a <span className="text-purple">Player</span>:</div>
+        <div>Level: <span className="font-medium">1</span></div>
+        <div>Score: <span className="font-medium">24.1k (28k / -3.9k)</span></div>
+        <div>Evaluations: <span className="font-medium">5 (4 Pos / 1 Neg)</span></div>
+        <div>Your Evaluation: <span className="font-medium text-pl3">Positive - High (3)</span></div>
+        <div>Your Evaluation <span className="font-medium text-pl3">Impact: 32%</span></div>
         <ReactECharts
           style={{ height: '110px' }}
           option={options2}
           className="body__chart w-3/4 mb-5 mt-2"
         />
       </div>
-      <hr />
       <div>
-        <div className="font-bold text-l">As a Subject:</div>
-        <div>Level: {auraLevel}</div>
+        <div className="font-bold text-l">As a <span className="text-orange">Subject</span>:</div>
+        <div>Level: <span className="font-medium">{auraLevel}</span></div>
         <div>
-          Score: {auraScore ? compactFormat(auraScore) : '-'} (28k / 4k)
+          Score: <span className="font-medium">{auraScore ? compactFormat(auraScore) : '-'} (28k / 4k)</span>
         </div>
         <div>
-          Evaluations: {inboundRatings !== null ? inboundRatings.length : '...'}{' '}
-          ({inboundRatingsStatsString})
+          Evaluations: {' '}
+          <span className="font-medium">{inboundRatings !== null ? inboundRatings.length : '...'}
+            ({inboundRatingsStatsString})</span>
         </div>
-        <div>Your Connection: {myConnectionToSubject?.level}</div>
+        <div>Your Connection: <span className="font-medium">{myConnectionToSubject?.level}</span></div>
         <div>
           Their Connection to you:{' '}
-          {outboundConnections !== null
+          <span className="font-medium">{outboundConnections !== null
             ? outboundConnections.find((c) => c.id === authData?.brightId)
-                ?.level || '-'
-            : '...'}
+            ?.level || '-'
+            : '...'}</span>
         </div>
         <div>
           Your Evaluation:{' '}
-          {loading ? (
+          <span className="font-medium">{loading ? (
             <span className="text-gray20">...</span>
           ) : Number(myRatingToSubject?.rating) > 0 ? (
-            <span className="text-green-800">
+            <span className="text-pl3">
               Positive - {myConfidenceValueInThisSubjectRating} (
               {Number(myRatingToSubject?.rating)})
             </span>
           ) : Number(myRatingToSubject?.rating) < 0 ? (
-            <span className="text-red-800">
+            <span className="text-nl3">
               Negative - {myConfidenceValueInThisSubjectRating} (
               {Number(myRatingToSubject?.rating)})
             </span>
           ) : (
             '-'
-          )}
+          )}</span>
         </div>
-        <div>Your Evaluation Impact: 32%</div>
+        <div>Your Evaluation Impact:
+          <span className="font-medium text-nl3">32%</span>
+        </div>
         <ReactECharts
           style={{ height: '110px' }}
           option={options2}
@@ -102,10 +104,10 @@ const CredibilityDetailsModal = ({
     <Modal isOpen={true} closeModalHandler={onClose} title={name}>
       <div>
         <CredibilityDetails subjectId={subjectId} />
-        <div className="mt-4 flex gap-2 text-black!">
+        <div className="mt-4 flex gap-2">
           <Link
             to={link}
-            className="btn btn--text-black w-full text-center text-black"
+            className="btn w-full text-center"
             onClick={(e) => {
               e.preventDefault();
               onClose();
@@ -116,7 +118,7 @@ const CredibilityDetailsModal = ({
           </Link>
           <Link
             to={link}
-            className="btn btn--text-black w-full text-center text-black"
+            className="btn btn--bg-orange w-full text-center"
             onClick={(e) => {
               e.preventDefault();
               onClose();
