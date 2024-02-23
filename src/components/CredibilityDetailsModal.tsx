3943f8518f9c4446bfde20581154942833b7a596
Mostafa Asadi <sam.21767@gmail.com>
2024-02-23 21:34:00 +0100

    updated toggle option for CredibilityDetailsModal

--- src/components/CredibilityDetailsModal.tsx
+++ src/components/CredibilityDetailsModal.tsx
@@ -6,13 +6,14 @@ import { useOutboundConnections } from 'hooks/useSubjectConnections';
 import { useSubjectInboundEvaluations } from 'hooks/useSubjectInboundEvaluations';
 import { useSubjectName } from 'hooks/useSubjectName';
 import { useSubjectVerifications } from 'hooks/useSubjectVerifications';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'store/hooks';
 import { selectAuthData } from 'store/profile/selectors';
 import { compactFormat } from 'utils/number';
 
 import { HorizontalProgressBar } from './Shared/HorizontalProgressBar';
+import { ToggleInputWithIcon } from './Shared/ToggleInput';
 
 const CredibilityDetails = ({ subjectId }: { subjectId: string }) => {
   const authData = useSelector(selectAuthData);
@@ -27,71 +28,120 @@ const CredibilityDetails = ({ subjectId }: { subjectId: string }) => {
   } = useMyEvaluationsContext(subjectId);
   const { outboundConnections } = useOutboundConnections(subjectId);
   const { options2 } = useContext(EchartsContext);
+  const [isSubject, setIsSubject] = useState(true);
 
   return (
     <div>
-      <div>
-        <div className="font-bold text-l">As a <span className="text-purple">Player</span>:</div>
-        <div>Level: <span className="font-bold">1</span></div>
-        <div className="flex w-full gap-2 items-center">
-          <div>Score: <span className="font-medium">24.1k</span></div>
-          <HorizontalProgressBar percentage={"w-[15%]"} />
-        </div>
-        <div>Evaluations: <span className="font-bold">5 (4 Pos / 1 Neg)</span></div>
-        <div>Your Evaluation: <span className="font-bold text-pl3">Positive - High (3)</span></div>
-        <div>Your Evaluation <span className="font-bold text-pl3">Impact: 32%</span></div>
-        <ReactECharts
-          style={{ height: '110px' }}
-          option={options2}
-          className="body__chart w-3/4 mb-5 mt-2"
-        />
-      </div>
-      <div>
-        <div className="font-bold text-l">As a <span className="text-orange">Subject</span>:</div>
-        <div>Level: <span className="font-bold">{auraLevel}</span></div>
+      <ToggleInputWithIcon
+        option1={'Subject'}
+        option2={'Player'}
+        icon1="/assets/images/Shared/brightid-icon.svg"
+        icon1Converted="/assets/images/Shared/brightid-icon-white.svg"
+        icon2="/assets/images/player.svg"
+        icon2Converted="/assets/images/player.svg"
+        isChecked={isSubject}
+        setIsChecked={setIsSubject}
+      />
+      {isSubject && (
         <div>
-          Score: <span className="font-medium">{auraScore ? compactFormat(auraScore) : '-'} (28k / 4k)</span>
+          <div className="font-bold text-l">
+            As a <span className="text-purple">Player</span>:
+          </div>
+          <div>
+            Level: <span className="font-bold">1</span>
+          </div>
+          <div className="flex w-full gap-2 items-center">
+            <div>
+              Score: <span className="font-medium">24.1k</span>
+            </div>
+            <HorizontalProgressBar percentage={'w-[15%]'} />
+          </div>
+          <div>
+            Evaluations: <span className="font-bold">5 (4 Pos / 1 Neg)</span>
+          </div>
+          <div>
+            Your Evaluation:{' '}
+            <span className="font-bold text-pl3">Positive - High (3)</span>
+          </div>
+          <div>
+            Your Evaluation{' '}
+            <span className="font-bold text-pl3">Impact: 32%</span>
+          </div>
+          <ReactECharts
+            style={{ height: '110px' }}
+            option={options2}
+            className="body__chart w-3/4 mb-5 mt-2"
+          />
         </div>
+      )}
+      {!isSubject && (
         <div>
-          Evaluations: {' '}
-          <span className="font-bold">{inboundRatings !== null ? inboundRatings.length : '...'}
-            ({inboundRatingsStatsString})</span>
-        </div>
-        <div>Your Connection: <span className="font-bold">{myConnectionToSubject?.level}</span></div>
-        <div>
-          Their Connection to you:{' '}
-          <span className="font-bold">{outboundConnections !== null
-            ? outboundConnections.find((c) => c.id === authData?.brightId)
-            ?.level || '-'
-            : '...'}</span>
-        </div>
-        <div>
-          Your Evaluation:{' '}
-          <span className="font-bold">{loading ? (
-            <span className="text-gray20">...</span>
-          ) : Number(myRatingToSubject?.rating) > 0 ? (
-            <span className="text-pl3">
-              Positive - {myConfidenceValueInThisSubjectRating} (
-              {Number(myRatingToSubject?.rating)})
+          <div className="font-bold text-l">
+            As a <span className="text-orange">Subject</span>:
+          </div>
+          <div>
+            Level: <span className="font-bold">{auraLevel}</span>
+          </div>
+          <div className="flex w-full gap-2 items-center">
+            <div>
+              Score:{' '}
+              <span className="font-medium">
+                {auraScore ? compactFormat(auraScore) : '-'}
+              </span>
+            </div>
+            <HorizontalProgressBar percentage={'w-[15%]'} />
+          </div>
+          <div>
+            Evaluations:{' '}
+            <span className="font-bold">
+              {inboundRatings !== null ? inboundRatings.length : '...'}(
+              {inboundRatingsStatsString})
             </span>
-          ) : Number(myRatingToSubject?.rating) < 0 ? (
-            <span className="text-nl3">
-              Negative - {myConfidenceValueInThisSubjectRating} (
-              {Number(myRatingToSubject?.rating)})
+          </div>
+          <div>
+            Your Connection:{' '}
+            <span className="font-bold">{myConnectionToSubject?.level}</span>
+          </div>
+          <div>
+            Their Connection to you:{' '}
+            <span className="font-bold">
+              {outboundConnections !== null
+                ? outboundConnections.find((c) => c.id === authData?.brightId)
+                    ?.level || '-'
+                : '...'}
             </span>
-          ) : (
-            '-'
-          )}</span>
+          </div>
+          <div>
+            Your Evaluation:{' '}
+            <span className="font-bold">
+              {loading ? (
+                <span className="text-gray20">...</span>
+              ) : Number(myRatingToSubject?.rating) > 0 ? (
+                <span className="text-pl3">
+                  Positive - {myConfidenceValueInThisSubjectRating} (
+                  {Number(myRatingToSubject?.rating)})
+                </span>
+              ) : Number(myRatingToSubject?.rating) < 0 ? (
+                <span className="text-nl3">
+                  Negative - {myConfidenceValueInThisSubjectRating} (
+                  {Number(myRatingToSubject?.rating)})
+                </span>
+              ) : (
+                '-'
+              )}
+            </span>
+          </div>
+          <div>
+            Your Evaluation Impact:
+            <span className="font-bold text-nl3">32%</span>
+          </div>
+          <ReactECharts
+            style={{ height: '110px' }}
+            option={options2}
+            className="body__chart w-3/4 mb-5 mt-2"
+          />
         </div>
-        <div>Your Evaluation Impact:
-          <span className="font-bold text-nl3">32%</span>
-        </div>
-        <ReactECharts
-          style={{ height: '110px' }}
-          option={options2}
-          className="body__chart w-3/4 mb-5 mt-2"
-        />
-      </div>
+      )}
     </div>
   );
 };
