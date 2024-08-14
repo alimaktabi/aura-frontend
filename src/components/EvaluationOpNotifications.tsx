5d60d71277bfbcbe11c8fae2dc6546bacccd14cc
Ali Mahdiyar <themonkeycoder@proton.me>
2024-08-14 22:21:40 +0330

    feat: connect operation notification ui

--- src/components/EvaluationOpNotifications.tsx
+++ src/components/EvaluationOpNotifications.tsx
@@ -21,6 +21,8 @@ export default function EvaluationOpNotifications() {
     }
   }, []);
 
+  const [notificationText, setNotificationText] = useState<string | null>(null);
+
   useEffect(() => {
     const prevOperations = prevOperationsRef.current;
     if (prevOperations) {
@@ -29,17 +31,19 @@ export default function EvaluationOpNotifications() {
           (op) => op.hash === operation.hash,
         );
         if (!prevOperation) {
-          alert(`Operation Submitted! Waiting for verification...`);
+          setNotificationText(
+            `Operation Submitted! Waiting for verification...`,
+          );
         } else if (
           prevOperation.state !== operation_states.APPLIED &&
           operation.state === operation_states.APPLIED
         ) {
-          alert(`Operation Applied!`);
+          setNotificationText(`Operation Applied!`);
         } else if (
           prevOperation.state !== operation_states.FAILED &&
           operation.state === operation_states.FAILED
         ) {
-          alert(`Operation Failed!`);
+          setNotificationText(`Operation Failed!`);
         }
       });
     }
@@ -49,23 +53,31 @@ export default function EvaluationOpNotifications() {
     localStorage.setItem('prevOperations', JSON.stringify(operations));
   }, [operations]);
 
-  return (
+  return notificationText === null ? (
+    <></>
+  ) : (
     <div className="card !bg-neutral-l2 !border-neutral-l3 w-full">
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-1">
           <div className="flex w-full justify-between items-center">
-            <img src="/assets/images/Shared/close-filled-red.svg" alt="" />
+            <img
+              src="/assets/images/Shared/close-filled-red.svg"
+              className="cursor-pointer"
+              onClick={() => setNotificationText(null)}
+              alt=""
+            />
             <p className="font-bold text-sm text-button-primary">
-              Failed to Submit Evaluation
+              {notificationText}
             </p>
           </div>
           <div className="flex w-full justify-between items-center">
-            <div className="flex gap-0.5">
-              <p className="font-medium text-sm text-nl3">Try Again</p>
-              <img src="/assets/images/Shared/refresh-red.svg" alt="" />
-            </div>
+            {/*TODO: Handle failed notifications*/}
+            {/*<div className="flex gap-0.5">*/}
+            {/*  <p className="font-medium text-sm text-nl3">Try Again</p>*/}
+            {/*  <img src="/assets/images/Shared/refresh-red.svg" alt="" />*/}
+            {/*</div>*/}
             <div
-              className="flex gap-0.5"
+              className="flex gap-0.5 cursor-pointer"
               onClick={() => setIsDetailed(!isDetailed)}
             >
               <p className="font-medium text-sm text-primary-d2">
@@ -83,29 +95,55 @@ export default function EvaluationOpNotifications() {
           </div>
         </div>
         {isDetailed && (
-          <div className="flex w-full justify-between items-center">
-            <div className="flex gap-1 items-center">
-              <img
-                src="/assets/images/profile.jpg"
-                className="border-solid border rounded w-4 h-4 border-orange"
-                alt=""
-              />
-              <div>Adam Stallard</div>
-              <img
-                src="/assets/images/Shared/brightid-icon.svg"
-                className="w-3 h-3"
-                alt=""
-              />
+          <>
+            <div className="flex w-full justify-between items-center">
+              <div className="flex gap-1 items-center">
+                <img
+                  src="/assets/images/profile.jpg"
+                  className="border-solid border rounded w-4 h-4 border-orange"
+                  alt=""
+                />
+                <div>Adam Stallard (hardcoded now)</div>
+                <img
+                  src="/assets/images/Shared/brightid-icon.svg"
+                  className="w-3 h-3"
+                  alt=""
+                />
+              </div>
+              <div className="rounded flex px-3 py-1 bg-pl3 gap-1 items-center">
+                <img
+                  src="/assets/images/Shared/thumbs-up-white.svg"
+                  className="w-3.5 h-3.5"
+                  alt=""
+                />
+                <span className="font-medium text-xs text-white">(+4)</span>
+              </div>
             </div>
-            <div className="rounded flex px-3 py-1 bg-pl3 gap-1 items-center">
-              <img
-                src="/assets/images/Shared/thumbs-up-white.svg"
-                className="w-3.5 h-3.5"
-                alt=""
-              />
-              <span className="font-medium text-xs text-white">(+4)</span>
+
+            <div className="flex w-full justify-between items-center">
+              <div className="flex gap-1 items-center">
+                <img
+                  src="/assets/images/profile.jpg"
+                  className="border-solid border rounded w-4 h-4 border-orange"
+                  alt=""
+                />
+                <div>Adam Stallard (hardcoded now)</div>
+                <img
+                  src="/assets/images/Shared/brightid-icon.svg"
+                  className="w-3 h-3"
+                  alt=""
+                />
+              </div>
+              <div className="rounded flex px-3 py-1 bg-pl3 gap-1 items-center">
+                <img
+                  src="/assets/images/Shared/thumbs-up-white.svg"
+                  className="w-3.5 h-3.5"
+                  alt=""
+                />
+                <span className="font-medium text-xs text-white">(+4)</span>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>
